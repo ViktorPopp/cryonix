764b5abafdd3ea2ac2b8ad3ba051d70f6394ebc7
Viktor Popp <viktorpopp.dev@gmail.com>
2025-05-16 15:29:06 +0200

    Restarted

--- src/plugins/hello.rs
+++
@@ -1,7 +0,0 @@
-use actix_web::{HttpResponse, Responder};
-
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body("<h1>Hello from the plugin!</h1>")
-}
